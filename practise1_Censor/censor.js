23fda3e8e46b1458c1100d47b67d02c3a0ea6ef6
Nikolay Kochetov <90146288+esthete014@users.noreply.github.com>
2022-10-15 22:29:18 +0300

    Практическое задание «Цензор»
    06.10.2022 - 15.10.2022

---
+++ practise1_Censor/censor.js
@@ -0,0 +1,31 @@
+function censor(){
+    const arr = []
+    return function f(str1, str2 = undefined) {
+        if (str2 == undefined) {
+            for (let temp of arr){
+                str1 = str1.replaceAll(temp.first, temp.second)
+            }
+            return str1
+        }
+        else {
+            arr.push({first: str1, second: str2})
+        }
+    };
+}
+
+
+
+const changeScene = censor();
+changeScene('PHP','JS');
+changeScene('backend', 'frontend')
+console.log(changeScene(document.getElementById("1").value, document.getElementById("2").value));
+
+let btn = document.getElementById("btn")
+btn.onclick = () => {
+    if (document.getElementById("2").value === ""){
+        alert(changeScene(document.getElementById("1").value))
+    }
+    else {
+        alert(changeScene(document.getElementById("1").value, document.getElementById("2").value))
+    }
+}
