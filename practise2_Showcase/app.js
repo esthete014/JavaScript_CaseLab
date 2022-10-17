17e5e61662952f601109cb3b192181d8116aa20d
Nikolay Kochetov <90146288+esthete014@users.noreply.github.com>
2022-10-17 23:15:45 +0300

    Showcase
    Практическое задание «Витрина» 11.10.2022 - 20.10.2022

---
+++ practise2_Showcase/app.js
@@ -0,0 +1,69 @@
+const products = [
+    {
+        id: 1,
+        title: 'Lenovo Yoga',
+        price: 3000,
+    },
+    {
+        id: 2,
+        title: 'Acer Aspire',
+        price: 1800,
+    },
+    {
+        id: 3,
+        title: 'Dell Vostro',
+        price: 3400
+    },
+];
+
+let order = [];
+
+function addToBasket(productId) {
+    // TODO: добавить проверку наличия товара в заказе (при наличии выдать alert, что товар уже в корзине)
+    let wasIt = false
+    for (let elem of order) {
+        wasIt = (elem.id === productId)
+        if (wasIt) { alert(elem.title + " has been add already!"); break }
+    }
+    // TODO: если товар еще не в корзине, добавить его из массива products
+    if (!wasIt){
+        order = [...order, products[productId - 1]]
+    }
+    // Эти строчки не трогаем, они отвечают за переотрисовку страницы
+    renderCart();
+    rerenderTotalPrice();
+}
+
+function removeFromBasket(productId) {
+    // TODO: описать логику удаления товара из корзины
+    for (let i = 0; i < order.length; i++){
+        if (order[i].id === productId){ console.log(i + " | " + order[i].id); order.splice(i, 1)}
+    }
+    // Эти строчки не трогаем, они отвечают за переотрисовку страницы
+    renderCart();
+    rerenderTotalPrice();
+}
+
+
+function rerenderTotalPrice() {
+    // TODO: опишите функционал подсчета общей стоимости заказа
+    let totalPrice = 0
+    for (let elem of order){
+        totalPrice += elem.price
+    }
+    // Не меняйте эту строчку
+    document.getElementById('total').innerText = totalPrice;
+}
+
+// Этот метод остается без изменений
+function renderCart() {
+    const cart = document.getElementById('basket-items');
+
+    cart.innerHTML = '';
+    order.forEach(item => {
+        const el = document.createElement('li');
+        el.innerText = item.title;
+        el.onclick = () => removeFromBasket(item.id);
+        cart.appendChild(el);
+    })
+}
